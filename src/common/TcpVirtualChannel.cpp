721d4eaa753570f3a07fb8430d8ddd118d4f3cea
Xigui Chen <xigui.chen@citrix.com>
2025-10-25 00:00:01 +0800

    only send two duplicate

--- src/common/TcpVirtualChannel.cpp
+++ src/common/TcpVirtualChannel.cpp
@@ -64,7 +64,6 @@ void TcpVirtualChannel::send(const char *data, size_t size)
         // Enqueue the data for multiple connections to make sure we can deliver it without problems
         sendQueue->enqueue(dataVec);
         sendQueue->enqueue(dataVec);
-        sendQueue->enqueue(dataVec);
     }
 }
 bool TcpVirtualChannel::isOpen() const
