74403dee2b491486759063eb1d49e55d9af99350
xigui chen <chenxigui_192930@163.com>
2026-05-10 15:53:56 +0800

    fix build issues for windows platform

--- src/common/Socket.h
+++ src/common/Socket.h
@@ -1,17 +1,17 @@
 #pragma once
 
 #ifdef _WIN32
+#include <windows.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
-#include <mswsock.h>
-typedef int ssize_t;  // Define ssize_t for Windows
-typedef SOCKET SocketFd;  // Use SOCKET type for Windows
+typedef int ssize_t;     // Define ssize_t for Windows
+typedef SOCKET SocketFd; // Use SOCKET type for Windows
 typedef SOCKADDR_IN sockaddr_in;
 typedef SOCKADDR sockaddr;
 // typedef int socklen_t;
 #pragma comment(lib, "ws2_32.lib")
-#pragma comment(lib, "mswsock.lib")
 
+#define SHUT_RDWR SD_BOTH
 
 // MIB_TCP_INFO structure for Windows
 #ifndef MIB_TCP_STATE_CLOSED
@@ -29,14 +29,13 @@ typedef SOCKADDR sockaddr;
 #define MIB_TCP_STATE_DELETE_TCB 12
 #endif
 #else
-#include <netinet/tcp.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <sys/socket.h>
 #include <fcntl.h>
-#include <poll.h>
+#include <netinet/in.h>
 #include <netinet/tcp.h>
-typedef int SocketFd;  // Use int type for Linux/macOS
+#include <poll.h>
+#include <sys/socket.h>
+typedef int SocketFd; // Use int type for Linux/macOS
 #endif
 
 // Socket creation and configuration
@@ -47,25 +46,24 @@ bool IsSocketNonBlocking(SocketFd socketFd);
 
 // Socket I/O operations
 ssize_t SendTcpData(SocketFd socketFd, const void *data, size_t length, int flags);
-ssize_t SendUdpData(SocketFd socketFd, const void *data, size_t length, int flags,
-                    const struct sockaddr *destAddr, socklen_t destAddrLen);
-ssize_t RecvUdpData(SocketFd socketFd, void *buffer, size_t bufferSize, int flags,
-                    struct sockaddr *srcAddr, socklen_t *srcAddrLen);
+ssize_t SendUdpData(SocketFd socketFd, const void *data, size_t length, int flags, const struct sockaddr *destAddr,
+                    socklen_t destAddrLen);
+ssize_t RecvUdpData(SocketFd socketFd, void *buffer, size_t bufferSize, int flags, struct sockaddr *srcAddr,
+                    socklen_t *srcAddrLen);
 ssize_t RecvTcpData(SocketFd socketFd, void *buffer, size_t bufferSize, int flags);
 
 // Non-blocking I/O operations with timeout
 ssize_t SendTcpDataNonBlocking(SocketFd socketFd, const void *data, size_t length, int flags, int timeoutMs);
 ssize_t SendUdpDataNonBlocking(SocketFd socketFd, const void *data, size_t length, int flags,
-                              const struct sockaddr *destAddr, socklen_t destAddrLen, int timeoutMs);
-ssize_t RecvUdpDataNonBlocking(SocketFd socketFd, void *buffer, size_t bufferSize, int flags,
-                              struct sockaddr *srcAddr, socklen_t *srcAddrLen, int timeoutMs);
+                               const struct sockaddr *destAddr, socklen_t destAddrLen, int timeoutMs);
+ssize_t RecvUdpDataNonBlocking(SocketFd socketFd, void *buffer, size_t bufferSize, int flags, struct sockaddr *srcAddr,
+                               socklen_t *srcAddrLen, int timeoutMs);
 ssize_t RecvTcpDataNonBlocking(SocketFd socketFd, void *buffer, size_t bufferSize, int flags, int timeoutMs);
 
 // Specialized receive functions
-ssize_t RecvTcpDataWithSize(SocketFd socketFd, void *buffer, size_t bufferSize,
-                            int flags, int bytesToRead);
-ssize_t RecvTcpDataWithSizeNonBlocking(SocketFd socketFd, void *buffer, size_t bufferSize,
-                                      int flags, int bytesToRead, int timeoutMs);
+ssize_t RecvTcpDataWithSize(SocketFd socketFd, void *buffer, size_t bufferSize, int flags, int bytesToRead);
+ssize_t RecvTcpDataWithSizeNonBlocking(SocketFd socketFd, void *buffer, size_t bufferSize, int flags, int bytesToRead,
+                                       int timeoutMs);
 
 // Socket control operations
 int SocketListen(SocketFd socketFd, int backlog);
@@ -84,12 +82,12 @@ bool IsSocketReadable(SocketFd socketFd, int timeoutMs);
 bool IsSocketWritable(SocketFd socketFd, int timeoutMs);
 int SocketBytesAvailable(SocketFd socketFd);
 
-
 // Error handling
 int SocketLogLastError();
 
 // Socket error codes
-enum SocketError {
+enum SocketError
+{
     SOCKET_ERROR_NONE = 0,
     SOCKET_ERROR_WOULD_BLOCK = -1,
     SOCKET_ERROR_TIMEOUT = -2,
