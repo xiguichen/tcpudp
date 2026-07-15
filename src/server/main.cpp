7c95d46324418c308f340aa4167d980dd04cf1af
Xigui Chen <xigui.chen@citrix.com>
2026-07-15 08:52:15 +0800

    fix: align ports to 7001, remove Wireguard secret leak, add Windows builds

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char* argv[]) {
     Log::getInstance().setLogLevel(LogLevel::LOG_INFO);
     SocketManager socketManager;
     socketManager.createSocket();
-    socketManager.bindToPort(6001);
+    socketManager.bindToPort(7001);
     socketManager.listenForConnections();
     socketManager.startTcpQueueToUdpThreadPool();
     socketManager.startUdpQueueToTcpThreadPool();
